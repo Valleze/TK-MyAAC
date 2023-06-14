fd9c7351f38d63c8e19795d2e99d87bf0940c038
Valleze <officialrodrigovalleze@gmail.com>
2023-06-14 19:02:18 -0300

    teste

--- system/migrations/19.php
+++
@@ -1,3 +0,0 @@
-<?php
-// this migration has been removed, but file kept for compability
-?>
--- templates/tibiacom/boxes/networks.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$twig->display('networks.html.twig', array(
-	'topPlayers' => getTopPlayers(5)
-));
